ea55e58f3ae3d1edb20fa4934b57bb65a5b51070
Giorgos Bouritsas <gbouritsas@gmail.com>
2018-03-11 21:32:21 +0200

    Added main_kernels.m

---
+++ face-pipeline/main_kernels.m
@@ -0,0 +1,10 @@
+% Moidfy this according to each movie
+movie_name='BMI'
+% Paths need to be modified
+result_dir  = ['/Data/gbouritsas_thesis/movies/' movie_name '/results'];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+addpath('compute-kernels');
+if ~exist(result_dir, 'dir')
+    mkdir(result_dir);
+end
+facedets_kernel_no_pconf_check(result_dir);
