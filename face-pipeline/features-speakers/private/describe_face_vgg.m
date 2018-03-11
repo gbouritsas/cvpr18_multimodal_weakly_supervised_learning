035695263aa9b88c7cf0b5a85a6c84404035473e
Giorgos Bouritsas <gbouritsas@gmail.com>
2018-03-11 23:15:09 +0200

    Modified describe_face_vgg.m

--- face-pipeline/features-speakers/private/describe_face_vgg.m
+++ face-pipeline/features-speakers/private/describe_face_vgg.m
@@ -2,7 +2,7 @@ function vggVec = describe_face_vgg(K,convNet)
 	%%%alignment:ABSOLUTELY NECESSARY SO THAT THE FACE IS IN THE CENTRE OF THE IMAGE
 	N       = size(K,1);
 	rect    = [N/3+1  N/3+1 N/3*2 N/3*2];
-	crop = lib.face_proc.faceCrop.crop(K,rect);
+	crop = faceCrop(K,rect);
 	crop= single(crop) ; % note: 255 range
 	%crop = imresize(crop, convNet.meta.normalization.imageSize(1:2)) ;
 	crop= bsxfun(@minus,crop,convNet.meta.normalization.averageImage) ;
@@ -10,6 +10,6 @@ function vggVec = describe_face_vgg(K,convNet)
 	res = vl_simplenn(convNet, crop) ;
 	feats = squeeze(gather(res(end).x)) ;
 	vggVec=feats/norm(feats);
-	
-	
+
+
 end
